0ea77364b870d18e1759d2d8a7b045ca349ea7ea
Vitaliy Zarubin <keygenqt@gmail.com>
2023-02-04 14:41:40 +0300

    Added Singleton

--- lib/src/patterns/creational/singleton/singleton.dart
+++ lib/src/patterns/creational/singleton/singleton.dart
@@ -1,10 +1,7 @@
 class Singleton {
   Singleton._();
-
   static Singleton? _instance;
-
   static Singleton get instance => _instance ??= Singleton._();
-
   String invokeFun() {
     return 'Invoke Fun Singleton';
   }
